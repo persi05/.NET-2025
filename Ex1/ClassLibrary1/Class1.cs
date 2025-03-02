80606a9af05a737725556d1b32262f15a2813766
Patryk Persak <abogusiac@gmail.com>
2025-03-02 18:48:41 +0100

    Added files

---
+++ Ex1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace MyServices
+{
+    public class Class1
+    {
+        public interface ILoggerService
+        {
+            void Log(string message);
+        }
+
+        public class ConsoleLogger : ILoggerService
+        {
+            public void Log(string message)
+            {
+                Console.WriteLine($"[LOG]: {message}");
+            }
+        }
+    }
+}
---
+++ Ex1/MyApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using MyLibrary;
+using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+using MyServices;
+using static MyServices.Class1;
+
+// Konfiguracja kontenera DI
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<ILoggerService, ConsoleLogger>()
+    .BuildServiceProvider();
+
+// Uzyskanie instancji loggera
+var logger = serviceProvider.GetService<ILoggerService>();
+logger.Log("Aplikacja uruchomiona.");
+
+Calculator calculator = new Calculator();
+
+int result1 = calculator.Add(6, 9);
+Console.WriteLine("Add result: " + result1);
+
+result1 = calculator.Subtract(21, 37);
+Console.WriteLine("Subtract result: " + result1);
+
+int sum = calculator.Add(5, 3);
+var result = new { Operation = "Add", A = 5, B = 3, Result = sum };
+string jsonResult = JsonConvert.SerializeObject(result, Formatting.Indented);
+Console.WriteLine(jsonResult);
+
+// Przykładowe użycie kalkulatora
+logger.Log($"Wynik dodawania: {sum}");
